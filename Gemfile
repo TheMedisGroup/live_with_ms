d5a36db1a18fde4ec3fe9c1f4f73dc550091e8f4
Michael Medis <TheMedisGroup@gmail.com>
2016-01-23 02:23:01 -0500

    Deleted 90% of exsisting React and began Implementing overhaul for a complete rails backend.

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'active_model_serializers'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -24,13 +24,16 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
+gem 'ffaker'
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'react-rails'
 # Use Unicorn as the app server
 # gem 'unicorn'
-
+gem 'socialization'
+gem 'paperclip'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -44,4 +47,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- client/components/layout/Menu.jsx
+++ client/components/layout/Menu.jsx
@@ -6,41 +6,42 @@ var HUMAN_CLIENT_ID = process.env['HUMAN_CLIENT_ID'];
 
 module.exports = React.createClass({
   handleSignOutLink: function() {
-    sessionStorage.setItem('jwt','');
-    location = '/';
+    var options = {
+      publicToken: 'PUBLIC_TOKEN_FOR_THE_USER'
+    }
+    HumanConnect.open(options);
   },
   handleClickLink: function(){
-    console.log("clicked human login button");
     var options = {
       clientUserId: encodeURIComponent('UNIQUE_ID_FOR_YOUR_USER'),
       clientId: HUMAN_CLIENT_ID, // grab it from app settings page
       publicToken: '',  // Leave blank for new users
 
       finish: function(err, sessionTokenObject) {
-          /* Called after user finishes connecting their health data
-           You need to post `sessionTokenObject` to your server to then:
-           1. Append your `clientSecret` to it
-           2. Send send it to our server for user credentials
+        /* Called after user finishes connecting their health data
+        You need to post `sessionTokenObject` to your server to then:
+        1. Append your `clientSecret` to it
+        2. Send send it to our server for user credentials
 
-           Sending a POST request with jQuery might look like this
-          (it's not necessary to use jQuery):
-          */
-          $.post('/your-servers-endpoint', sessionTokenObject, function(res){
+        Sending a POST request with jQuery might look like this
+        (it's not necessary to use jQuery):
+        */
+        $.post('/your-servers-endpoint', sessionTokenObject, function(res){
 
-          });
+        });
 
-          // Include code here to refresh the page.
+        // Include code here to refresh the page.
 
       },
       close: function() {
-          /* Optional callback called when a user closes the popup
-             without connecting any data sources */
+        /* Optional callback called when a user closes the popup
+        without connecting any data sources */
       },
       error: function(err) {
-          /* Optional callback called if an error occurs when loading
-             the popup. */
+        /* Optional callback called if an error occurs when loading
+        the popup. */
 
-          // `err` has fields: `code`, `message`, `detailedMessage`
+        // `err` has fields: `code`, `message`, `detailedMessage`
       }
     }
     HumanConnect.open(options);
@@ -48,29 +49,28 @@ module.exports = React.createClass({
   render: function() {
     if (this.props.signedIn) {
       var signingLink = <li><span onClick={this.handleSignOutLink}>logout</span></li>;
-    } else{
-      var signingLink = <li><a href={this.props.origin + '/request_token'}>login</a></li>;
-    }
-    return (
-      <div id="menu">
-        <span id="menu-link" onClick={this.props.sendMenuClick}><span></span></span>
-        <div id="menu-list">
-          <div className="pure-menu pure-menu-open">
-            <span className="pure-menu-heading"></span>
-            <ul>
-              <li><Link to="search">Search</Link></li>
-              <li><Link to="about">About</Link></li>
-              <li><div className="rollover"> <a target="_blank" target="http://www.nationalmssociety.org/"></a> </div></li>
-              <li><div className="rolloverlogo"> <a target="_blank" target="https://github.com/mikeMedis"></a> </div></li>
-              {signingLink}
-              <li><span onClick={this.handleClickLink}>Login w/ Human</span></li>
-            </ul>
-            <p></p>
-            <p></p>
-            <div className="rolloverlogo"> <a href="https://github.com/mikeMedis"></a> </div>
-        </div>
-        </div>
-      </div>
-    );
-  }
-});
+      } else{
+        var signingLink = <li><a href={this.props.origin + '/request_token'}>login</a></li>;
+        }
+        return (
+          <div id="menu">
+            <span id="menu-link" onClick={this.props.sendMenuClick}><span></span></span>
+            <div id="menu-list">
+              <div className="pure-menu pure-menu-open">
+                <span className="pure-menu-heading"></span>
+                <ul>
+                  <li><Link to="search">Search</Link></li>
+                  <li><Link to="about">About</Link></li>
+                  <li><div className="rollover"> <a target="_blank" target="http://www.nationalmssociety.org/"></a> </div></li>
+                  <li><div className="rolloverlogo"> <a target="_blank" target="https://github.com/mikeMedis"></a> </div></li>
+                  {signingLink}
+                  <li><span onClick={this.handleClickLink}><img id='connect-health-data-btn' src='https://connect.humanapi.co/assets/button/blue.png'/></span></li>
+                </ul>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+          </div>
+          );
+        }
+      });
